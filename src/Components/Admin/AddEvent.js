73a2dc2f44240a8286ce7c13aebfbe59a5dc5386
my name <you@exampl.com>
2020-10-04 21:42:58 +0600

    admin addEvent page submiting data into database

--- src/Components/Admin/AddEvent.js
+++ src/Components/Admin/AddEvent.js
@@ -1,30 +1,79 @@
 import React, { useState } from 'react'
 
 const AddEvent = () => {
-    const [formInfo, setFormInfo] = useState({})
+    const [formInfo, setFormInfo] = useState({
+        name: '', description: '', date: '', image: 'libraryBooks.png', color: '#badc58'
+    })
+    const [errorMessage, setErrorMessage] = useState({})
+
+    const collectFormInfo = (e) => {
+        const name = e.target.name
+        const value = e.target.value
+        if (name === 'title') {
+            (value.length >= 10) ?
+                crossCheck('name', value, 'name', '') : crossCheck('name', '', 'name', 'Insert a meaningful event title')
+        }
+        else if (name === 'description') {
+            setFormInfo({ ...formInfo, description: value })
+        }
+        else if (name === 'date') {
+            setFormInfo({ ...formInfo, date: value })
+        }
+    }
+    const crossCheck = (infoKey, infoData, errKey, errData) => {
+        setFormInfo({ ...formInfo, [infoKey]: infoData })
+        setErrorMessage({ ...errorMessage, [errKey]: errData })
+    }
+    const addEventToDatabase = (e) => {
+        e.preventDefault()
+        const { name, date } = formInfo
+        if (!date) {
+            setErrorMessage({ ...errorMessage, date: 'Please insert a date' })
+            return
+        }
+        else
+            setErrorMessage({ ...errorMessage, date: '' })
+
+        if (name) {
+            fetch('http://localhost:4000/adminInsertEvent', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(formInfo)
+            })
+                .then(res => res.json())
+                .then(result => {
+                    setErrorMessage({ message: `${result.insertedCount} new event(s) added successfully` })
+                })
+        }
+        else setErrorMessage({ ...errorMessage, message: 'Fields with \'*\' sign in required' })
+    }
 
     return (
         <section className="add-event px-3">
             <h4 className="py-3">Add Event</h4>
-            <form className="row">
+            <form className="row" onSubmit={addEventToDatabase}>
                 <div className="col-md-6">
                     <div className="d-flex flex-column px-4">
                         <label className="m-0">Event Title</label>
-                        <input type="text" />
+                        <input name="title" type="text" placeholder="Event Title" onBlur={collectFormInfo} />
+                        <span className="error">{errorMessage.name}</span>
                     </div>
-                    <div className="d-flex flex-column px-4 mt-4">
+                    <div className="d-flex flex-column px-4 mt-2">
                         <label className="m-0">Description</label>
-                        <textarea name="" cols="30" rows="10"></textarea>
+                        <textarea name="description" cols="30" rows="10" placeholder="Event Description" onBlur={collectFormInfo}></textarea>
+                        <span className="text-danger">{errorMessage.message}</span>
                     </div>
                 </div>
                 <div className="col-md-6">
                     <div className="d-flex flex-column px-4">
                         <label className="m-0">Event Date</label>
-                        <input type="date" />
+                        <input name="date" type="date" onChange={collectFormInfo} />
+                        <span className="error">{errorMessage.date}</span>
                     </div>
-                    <div className="d-flex flex-column px-4 mt-4">
+                    <div className="d-flex flex-column px-4 mt-2">
                         <label className="m-0">Event Title</label>
-                        <input type="file" disabled />
+                        <input name="file" type="file" disabled />
+                        <span className="error">Add title and date and submit it. Description optional</span>
                     </div>
                 </div>
                 <input type="submit" value="Add Event" />
