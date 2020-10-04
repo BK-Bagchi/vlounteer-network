5da316be49c972dcb84cba3e98347d6089f6c4db
my name <you@exampl.com>
2020-10-04 22:23:07 +0600

    menu color dynamic onClick added at admin

--- src/Components/Admin/Admin.js
+++ src/Components/Admin/Admin.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Admin.css'
 import { Link, Route, Switch, useHistory, useRouteMatch } from 'react-router-dom'
 import Logo from '../../Resources/logos/Group1329.png'
@@ -9,14 +9,21 @@ import PeopleAltIcon from '@material-ui/icons/PeopleAlt'
 
 const Admin = () => {
     const history = useHistory()
+    const [color, setColor] = useState(['royalblue', 'black'])
     let { path, url } = useRouteMatch()
+    const colorControl = (one, two) => setColor([one, two])
+
     return (
         <section className="row w-100 m-0 px-4 py-2 admin">
             <div className="col-3 p-0">
                 <img className="w-100" src={Logo} alt="Logo" onClick={() => history.push('/')} />
                 <ul className="p-3">
-                    <Link to={`${url}/volunteerList`}><li><PeopleAltIcon /> Volunteer register list</li></Link>
-                    <Link to={`${url}/addEvent`}><li><AddIcon /> Add Event</li></Link>
+                    <Link to={`${url}/volunteerList`} onClick={() => colorControl('royalblue', 'black')}>
+                        <li style={{ color: color[0] }}><PeopleAltIcon /> Volunteer register list</li>
+                    </Link>
+                    <Link to={`${url}/addEvent`} onClick={() => colorControl('black', 'royalblue')}>
+                        <li style={{ color: color[1] }}><AddIcon /> Add Event</li>
+                    </Link>
                 </ul>
             </div>
             <div className="col-9 p-0">
